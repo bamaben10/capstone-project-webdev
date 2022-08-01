5caf844fcf4a92081a9a0573283c891151864321
Benjamin Salmon <bensalmon8@gmail.com>
2022-08-01 17:15:40 -0400

    working on components

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+    return(
+        <div className='Body'>
+            <h1>Hello World</h1>            
+        </div>
+    )
+}
+
+export default Home;
