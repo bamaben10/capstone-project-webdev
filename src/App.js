a4840f061824b74ae25a60ed7d7d61338b934f38
Benjamin Salmon <bensalmon8@gmail.com>
2022-08-02 17:50:55 -0400

    server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import Header from './components/Header';
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import "./App.css";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={ <Home /> } />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import SearchBar from '../SearchBar';
 // import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     return(
         <div className='Body'>
-            <h1>Hello World</h1>            
+            <SearchBar />    
         </div>
     )
 }
